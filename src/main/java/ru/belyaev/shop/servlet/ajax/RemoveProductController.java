de5f1b1eea144e0d4410559fc9e685cc9dcb260c
Alex <luxa4job@gmail.com>
2020-02-29 12:39:34 +0300

    delete deprecated classes

--- src/main/java/ru/belyaev/shop/servlet/ajax/RemoveProductController.java
+++ src/main/java/ru/belyaev/shop/servlet/ajax/RemoveProductController.java
@@ -9,20 +9,18 @@ package ru.belyaev.shop.servlet.ajax;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import ru.belyaev.shop.form.ProductForm;
 import ru.belyaev.shop.model.ShoppingCart;
 import ru.belyaev.shop.servlet.AbstractController;
 import ru.belyaev.shop.util.SessionUtil;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
-public class RemoveProductController extends AbstractProductController {
+public class RemoveProductController extends AbstractController {
     private static final long serialVersionUID = -3046216247699203961L;
 
     @RequestMapping(value = "/ajax/json/product/remove")
